2dc275b153b317f79e4094043192a65a7d9700cf
Anderson Foschiera <foschieraanderson@gmail.com>
2022-03-01 16:17:57 -0300

    Status from operation added in tags.

--- app/tags.py
+++ app/tags.py
@@ -2,10 +2,11 @@ import typer
 from rich.console import Console
 from rich.table import Table
 from enum import Enum
-from typing import List, Optional
 
 from models.tag_model import Tag
 from repositories.tag_repository import create, delete, list_all, update
+
+from time import sleep
 
 console = Console()
 app = typer.Typer()
@@ -21,23 +22,26 @@ class ColorsTag(str, Enum):
 
 @app.command(short_help='Add an tag')
 def add(name: str, color: ColorsTag = ColorsTag.white):
-    tag = Tag(id=None, name=name, color=color)
-    create(tag)
-    show()
+    with console.status(f'Adding [bold green]tag {name}[/]...'):
+        tag = Tag(id=None, name=name, color=color)
+        create(tag)
+        show()
 
 @app.command(short_help='Remove an tag')
 def rm(key: int):
-    console.print("KEY: ", key)
-    delete(key=key)
-    show()
+    with console.status(f'Removing [bold red]tag {key}[/]...'):
+        delete(key=key)
+        show()
 
 @app.command(short_help='Update an tag')
 def up(key: int, name: str = None, color: ColorsTag = None):
-    update(key=key, name=name, color=color)
-    show()
+    with console.status(f'Updating [bold cyan]tag {key}[/]...'):
+        update(key=key, name=name, color=color)
+        show()
 
 @app.command(short_help='List all tags')
 def show():
+    sleep(1)
     tags = list_all()
     table = Table(title='My Tags', title_justify='center', show_header=True, header_style='bold blue')
     table.add_column('#', style='dim', width=4)
@@ -46,5 +50,6 @@ def show():
 
     for tag in tags:
         table.add_row(str(tag.id), tag.name, f'[{tag.color}]{tag.color}[/{tag.color}]')
+    console.clear()
     console.print(table)
 
