bbc92d8f07917fffa34413bfcb841504403f0acb
Anderson Foschiera <foschieraanderson@gmail.com>
2022-03-01 18:43:35 -0300

    Using rich methods for print the exceptions.

--- app/tags.py
+++ app/tags.py
@@ -24,36 +24,48 @@ class ColorsTag(str, Enum):
 @app.command(short_help='Add an tag')
 def add(name: str, color: ColorsTag = ColorsTag.white):
     with console.status(f'[green]:floppy_disk: Adding [bold]tag {name}[/]...[/]'):
-        tag = Tag(id=None, name=name, color=color)
-        create(tag)
-        show()
+        try:
+            tag = Tag(id=None, name=name, color=color)
+            create(tag)
+            show()
+        except Exception:
+            console.print_exception(show_locals=True)
 
 @app.command(short_help='Remove an tag')
 def rm(key: int):
     confirm = Confirm.ask(f'[red]:warning: Do you want to remove tag [bold]{key}[/] :question:[/]')
     if confirm:
         with console.status(f'[red]:wastebasket: Removing [bold]tag {key}[/]...[/]'):
-            delete(key=key)
-            show()
+            try:
+                delete(key=key)
+                show()
+            except Exception:
+                console.print_exception(show_locals=True)
 
 
 @app.command(short_help='Update an tag')
 def up(key: int, name: str = None, color: ColorsTag = None):
     with console.status(f'[cyan]:recycle: Updating [bold]tag {key}[/]...[/]'):
-        update(key=key, name=name, color=color)
-        show()
+        try:
+            update(key=key, name=name, color=color)
+            show()
+        except Exception:
+            console.print_exception(show_locals=True)
 
 @app.command(short_help='List all tags')
 def show():
     sleep(1)
-    tags = list_all()
-    table = Table(title='My Tags', title_justify='center', show_header=True, header_style='bold blue')
-    table.add_column('#', style='dim', width=4)
-    table.add_column('Tag', min_width=20, justify='center')
-    table.add_column('Color', min_width=10, justify='center')
+    try:
+        tags = list_all()
+        table = Table(title='My Tags', title_justify='center', show_header=True, header_style='bold blue')
+        table.add_column('#', style='dim', width=4)
+        table.add_column('Tag', min_width=20, justify='center')
+        table.add_column('Color', min_width=10, justify='center')
 
-    for tag in tags:
-        table.add_row(str(tag.id), tag.name, f'[{tag.color}]{tag.color}[/{tag.color}]')
-    console.clear()
-    console.print(table)
+        for tag in tags:
+            table.add_row(str(tag.id), tag.name, f'[{tag.color}]{tag.color}[/{tag.color}]')
+        console.clear()
+        console.print(table)
+    except Exception:
+        console.print_exception(show_locals=True)
 
