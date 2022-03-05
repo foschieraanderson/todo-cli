3cf48d6b042973b59a7ecbc97c5df6acb940a9d2
Anderson Foschiera <foschieraanderson@gmail.com>
2022-03-04 22:09:16 -0300

    Added tasks on main.

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import typer
-from app import tags
+from app import tasks, tags
 
 app = typer.Typer()
 
+app.add_typer(tasks.app, name='tasks')
 app.add_typer(tags.app, name='tags')
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app()
