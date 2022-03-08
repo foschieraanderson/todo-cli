161b847c0c2988caa0dd9b18dc83359735906e9b
Anderson Foschiera <foschieraanderson@gmail.com>
2022-03-08 03:54:19 -0300

    Added table show in tasks.

--- app/tasks.py
+++ app/tasks.py
@@ -1,9 +1,11 @@
 from enum import Enum
+from datetime import datetime 
 from models.task_model import Task
 from repositories.task_repository import clear_all, complete, create, delete, list_all
 import typer
 from rich.console import Console
 from rich.prompt import Confirm
+from rich.table import Table
 
 console = Console()
 app = typer.Typer()
@@ -12,27 +14,41 @@ app = typer.Typer()
 def add(title: str, description: str = '', tag: str = ''):
     task = Task(id=None, title=title, description=description, tag=tag)
     create(task)
-    console.print(f'Task {task}')
+    show()
 
 @app.command(short_help='Complete an task')
 def done(key: int, done: bool = True):
     complete(key=key, done=done)
-    console.print(f'KEY: {key} - DONE: {done}')
+    show()
 
 @app.command(short_help='Remove an task')
 def rm(key: int):
     confirm = Confirm.ask(f'[red] :warning: Do you want to remove task [bold]{key}[/] :question:[/]')
     if confirm:
         delete(key=key)
-        console.print('KEY: ', key)
+        show()
 
 @app.command(short_help='Remove all tasks')
 def clear():
     confirm = Confirm.ask(f'[white on red] :warning: Do you want to remove [bold]all[/] tasks :question:[/]')
     if confirm:
         clear_all()
+        show()
 
 @app.command(short_help='List all tasks')
 def show():
     tasks = list_all()
-    console.print('TASKS: ', tasks)
+    table = Table(title='My Tasks', title_justify='center', show_header=True, header_style='bold blue')
+    table.add_column('#', style='dim')
+    table.add_column('Title', min_width=20, justify='left')
+    table.add_column('Tag', min_width=15, justify='left')
+    table.add_column('Created', justify='left')
+    table.add_column('Completed', justify='left')
+    table.add_column('Done', justify='center')
+
+    for task in tasks:
+        done = '[green]✅[/]' if task.done else '[red]:negative_squared_cross_mark:[/]'
+        created = datetime.fromisoformat(task.created_at).strftime('%d/%m/%Y')
+        completed = datetime.fromisoformat(task.completed_at).strftime('%d/%m/%Y') if task.completed_at else None
+        table.add_row(str(task.id), task.title, task.tag, created, completed, done)
+    console.print(table)
