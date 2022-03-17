23e773f3216ba9b7440a031d2e639bf337b99b5d
Anderson Foschiera <foschieraanderson@gmail.com>
2022-03-16 23:59:13 -0300

    Added version command.

---
+++ configs/__init__.py
@@ -0,0 +1,2 @@
+__app_name__ = 'todo-cli'
+__version__ = '0.1.0'
--- main.py
+++ main.py
@@ -1,7 +1,28 @@
 import typer
+from typing import Optional
+from configs import __app_name__, __version__
 from app import tasks, tags
 
 app = typer.Typer()
+
+def _version_callback(value: bool) -> None:
+    if value:
+        typer.echo(f'{__app_name__} v{__version__}')
+        raise typer.exit()
+
+app.callback()
+def main(
+    version: Optional[bool] = typer.Option(
+        None,
+        '--version',
+        '-v',
+        help='Show the application version and exit.',
+        callback=_version_callback,
+        is_eager=True,
+    )
+) -> None:
+    return
+
 
 app.add_typer(tasks.app, name='tasks')
 app.add_typer(tags.app, name='tags')
