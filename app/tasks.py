0657e96ba7041b37be90d97fef33918e1b1d9e6f
Anderson Foschiera <foschieraanderson@gmail.com>
2022-03-18 23:49:26 -0300

    Status of loading while the actions is executing.

--- app/tasks.py
+++ app/tasks.py
@@ -1,3 +1,4 @@
+from time import sleep
 from enum import Enum
 from utils.helpers import date_format
 from models.task_model import Task
@@ -17,53 +18,59 @@ tags = Enum('Tags', tag_options)
 
 @app.command(short_help='Add an task')
 def add(title: str, description: str = '', tag: tags = None):
-    try:
-        tag_value = tag.value if tag else ''
-        task = Task(id=None, title=title, description=description, tag=tag_value)
-        create(task)
-        show()
-    except Exception:
-        console.print_exception(show_locals=True)
+    with console.status(f'[green] :floppy_disk: Adding [bold]tag {title}[/]...[/]'):
+        try:
+            tag_value = tag.value if tag else ''
+            task = Task(id=None, title=title, description=description, tag=tag_value)
+            create(task)
+            show()
+        except Exception:
+            console.print_exception(show_locals=True)
 
 @app.command(short_help='Update an task')
 def up(key: int, title: str = None, description: str = '', tag: tags = None):
-    try:
-        tag_value = tag.value if tag else ''
-        update(key=key, title=title, description=description, tag=tag_value)
-        show()
-    except Exception:
-        console.print_exception(show_locals=True)
+    with console.status(f'[cyan] :recycle: Updating [bold]tag {key}[/]...[/]'):
+        try:
+            tag_value = tag.value if tag else ''
+            update(key=key, title=title, description=description, tag=tag_value)
+            show()
+        except Exception:
+            console.print_exception(show_locals=True)
 
 @app.command(short_help='Complete an task')
 def done(key: int, done: bool = True):
-    try:
-        complete(key=key, done=done)
-        show()
-    except Exception:
-        console.print_exception(show_locals=True)
+    with console.status(f'[cyan] :recycle: Completing [bold]tag {key}[/]...[/]'):
+        try:
+            complete(key=key, done=done)
+            show()
+        except Exception:
+            console.print_exception(show_locals=True)
 
 @app.command(short_help='Remove an task')
 def rm(key: int):
     confirm = Confirm.ask(f'[red] :warning: Do you want to remove task [bold]{key}[/] :question:[/]')
     if confirm:
-        try:
-            delete(key=key)
-            show()
-        except Exception:
-            console.print_exception(show_locals=True)
+        with console.status(f'[red]:wastebasket: Removing [bold]tag {key}[/]...[/]'):
+            try:
+                delete(key=key)
+                show()
+            except Exception:
+                console.print_exception(show_locals=True)
 
 @app.command(short_help='Remove all tasks')
 def clear():
     confirm = Confirm.ask(f'[white on red] :warning: Do you want to remove [bold]all[/] tasks :question:[/]')
     if confirm:
-        try:
-            clear_all()
-            show()
-        except Exception:
-            console.print_exception(show_locals=True)
+        with console.status(f'[red] :wastebasket: Removing [bold]all tags[/]...[/]'):
+            try:
+                clear_all()
+                show()
+            except Exception:
+                console.print_exception(show_locals=True)
 
 @app.command(short_help='List all tasks')
 def show():
+    sleep(1)
     try:
         tasks = list_all()
         table = Table(title='My Tasks', title_justify='center', show_header=True, header_style='bold blue')
