4e3eccdd9cf0a271ba03cd6f24935628df741da8
Anderson Foschiera <foschieraanderson@gmail.com>
2022-03-22 22:14:57 -0300

    Return connection with database.

--- configs/database.py
+++ configs/database.py
@@ -1,23 +1,25 @@
 import sqlite3
-  
-try:
-    
-    # Connect to DB and create a cursor
-    conn = sqlite3.connect('sql.db')
-    cursor = conn.cursor()
-    # print('DB Init')
-  
-    # Close the cursor
-    # cursor.close()
-  
-# Handle errors
-except sqlite3.Error as error:
-    print('Error occured - ', error)
-  
-# Close DB Connection irrespective of success
-# or failure
-# finally:
-    
-#     if conn:
-#         conn.close()
-#         print('SQLite Connection closed')
+ 
+def connection():
+    try:
+        
+        # Connect to DB and create a cursor
+        conn = sqlite3.connect('sql.db')
+        cursor = conn.cursor()
+        # print('DB Init')
+      
+        return cursor
+        # Close the cursor
+        # cursor.close()
+      
+    # Handle errors
+    except sqlite3.Error as error:
+        print('Error occured - ', error)
+      
+    # Close DB Connection irrespective of success
+    # or failure
+    # finally:
+        
+    #     if conn:
+    #         conn.close()
+    #         print('SQLite Connection closed')
