1b0331e10054facc1fb5f71d1dbf5cce9514796d
Anderson Foschiera <foschieraanderson@gmail.com>
2022-03-26 06:09:43 -0300

    Move the show table and tag options to help functions.

--- app/utils/helpers.py
+++ app/utils/helpers.py
@@ -1,6 +1,59 @@
+from time import sleep
+from enum import Enum
 from datetime import datetime
+from typing import List
+
+import typer
+from rich.console import Console
+from rich.table import Table
+from rich.padding import Padding
+
+from app.repositories.tag_repository import list_all as all_tags
+from app.models.task_model import Task
+
+console = Console()
 
 def date_format(date: str) -> str:
-    """Format datime to date"""
+    """Format datetime to date"""
     date = datetime.fromisoformat(date).strftime('%d/%m/%Y')
     return date
+
+def get_tags() -> Enum:
+    """Return tag options"""
+    tag_options = {tag.name: tag.name for tag in all_tags()}
+    tags = Enum('Tags', tag_options)
+    return tags
+
+
+def show_table_tasks(tasks: List[Task]) -> None:
+    """List all tasks in table"""
+    sleep(1)
+    if tasks:
+        table = Table(title='My Tasks', title_justify='center', show_header=True, header_style='bold blue')
+        table.add_column('#', style='dim')
+        table.add_column('Title', min_width=20, justify='left')
+        table.add_column('Tag', justify='center')
+        table.add_column('Created', justify='center')
+        table.add_column('Completed', justify='center')
+        table.add_column('Done', justify='center')
+
+        colors = {tag.name: tag.color for tag in all_tags()}
+
+        for task in tasks:
+            pk = f'{task.id}'
+            title = f'[strike]{task.title}[/]' if task.done else task.title
+            tag = Padding(
+                f'{task.tag}',
+                (0,1),
+                style=f'black on {colors[task.tag]}',
+                expand=True
+            ) if task.tag else '-----'
+            created = date_format(task.created_at)
+            completed = date_format(task.completed_at) if task.done else '-----'
+            done = '[green]✅[/]' if task.done else '[red]:negative_squared_cross_mark:[/]'
+
+            table.add_row(pk, title, tag, created, completed, done)
+        console.clear()
+        console.print(table)
+    else:
+        console.print('[i]Nenhuma task encontrada...[/i]')
